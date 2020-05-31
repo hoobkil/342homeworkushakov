8679a2bd8e7c20359a3b3836816951f45b373e08
f.ushakov <fyshakov@gmail.com>
2020-05-31 05:34:06 +0300

    added color changing spinner

--- app/src/main/java/com/example/a332homeworkushakov/MainActivity.java
+++ app/src/main/java/com/example/a332homeworkushakov/MainActivity.java
@@ -16,46 +16,71 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
-    private Spinner spinner;
+    private Spinner languageSpinner;
+    private Spinner colorSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
+
         init();
         initSpinner();
 
     }
 
     public void init(){
-        spinner = findViewById(R.id.spinner);
-        button  = findViewById(R.id.button);
+        languageSpinner = findViewById(R.id.languageSpinner);
+        colorSpinner = findViewById(R.id.colorSpinner);
+        button  = findViewById(R.id.okBtnLanguage);
         textView = findViewById(R.id.textView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(spinner.getSelectedItem().toString().contains("Русский")){
+                if(languageSpinner.getSelectedItem().toString().contains("Русский")){
                     Locale locale = new Locale("ru");
                     Configuration config = new Configuration();
                     config.setLocale(locale);
                     getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                    if(colorSpinner.getSelectedItem().toString().contains("Черный")){
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_DEFAULT);
+                    } else if (colorSpinner.getSelectedItem().toString().contains("Синий")){
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLUE);
+                    } else if (colorSpinner.getSelectedItem().toString().contains("Зеленый")) {
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_GREEN);
+                    }
                     recreate();
-                } else if (spinner.getSelectedItem().toString().contains("English")){
+                } else if (languageSpinner.getSelectedItem().toString().contains("English")){
                     Locale locale = new Locale("en");
                     Configuration config = new Configuration();
                     config.setLocale(locale);
                     getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                    if(colorSpinner.getSelectedItem().toString().contains("Черный")){
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_DEFAULT);
+                    } else if (colorSpinner.getSelectedItem().toString().contains("Синий")){
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLUE);
+                    } else if (colorSpinner.getSelectedItem().toString().contains("Зеленый")) {
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_GREEN);
+                    }
                     recreate();
                 }
             }
         });
+
+
     }
 
     private void initSpinner(){
-        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this, R.array.language, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        ArrayAdapter<?> languageAdapter = ArrayAdapter.createFromResource(this, R.array.language, android.R.layout.simple_spinner_item);
+        languageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(languageAdapter);
+
+        ArrayAdapter<?> colorAdapter = ArrayAdapter.createFromResource(this, R.array.color, android.R.layout.simple_spinner_item);
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        colorSpinner.setAdapter(colorAdapter);
+
 
     }
 
